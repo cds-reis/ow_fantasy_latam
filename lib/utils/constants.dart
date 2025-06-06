b1a4d6f7f2ddc67499c1a6e99ecc73d7baccff89
cds-reis <114937464+cds-reis@users.noreply.github.com>
2025-06-06 01:43:12 -0300

    removing const assert (#6)

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -3,14 +3,6 @@ class ConstAssert {
     : assert(valid, message);
 }
 
-@ConstAssert(
-  valid: supabaseUrl != '',
-  message: 'SUPABASE_URL is not set',
-)
 const supabaseUrl = String.fromEnvironment('SUPABASE_URL');
 
-@ConstAssert(
-  valid: supabaseAnonKey != '',
-  message: 'SUPABASE_ANON_KEY is not set',
-)
 const supabaseAnonKey = String.fromEnvironment('SUPABASE_ANON_KEY');
