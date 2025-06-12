f031e7c60557ae225f80561950dcb981b7114e6b
Rafael Reis <rafaelcdsreis@gmail.com>
2025-06-12 18:48:38 -0300

    fix

---
+++ lib/providers/teams_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'teams_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$teamsHash() => r'd706d6df9c7c34c3c0134d4f9e5e84a4181a7604';
+
+/// See also [teams].
+@ProviderFor(teams)
+final teamsProvider = AutoDisposeFutureProvider<IList<Team>>.internal(
+  teams,
+  name: r'teamsProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$teamsHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef TeamsRef = AutoDisposeFutureProviderRef<IList<Team>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
