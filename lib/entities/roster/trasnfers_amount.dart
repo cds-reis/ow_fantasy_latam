3e74875524d89e2ae2ec4defe4d3e4e78f05c212
Rafael Reis <rafaelcdsreis@gmail.com>
2025-06-16 15:27:43 -0300

    wip

--- lib/entities/roster/trasnfers_amount.dart
+++ lib/entities/roster/trasnfers_amount.dart
@@ -1,5 +1,5 @@
 extension type const TransfersAmount(int value) {
   static const TransfersAmount zero = TransfersAmount(0);
 
-  bool get hasSpace => value > 0;
+  bool hasSpace(int amountOfChanges) => value > amountOfChanges;
 }
